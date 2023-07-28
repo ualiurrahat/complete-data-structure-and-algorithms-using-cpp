749fb752923fb588b49eac3d46b87522080585e1
ualiurrahat <ualiurrahat25@gmail.com>
2023-07-28 17:12:55 +0600

    linear search,union,inter-section of arrays coded

---
+++ 6. Arrays/11_intersectionOfTwoSortedArrays.cpp
@@ -0,0 +1,15 @@
+/*
+find intersection of two sorted arrays.
+ex: a[] = [1,2,2,3,3,4,5,6]
+b[] =[2,3,3,5,6,7]
+output = [2,3,3,5,6]
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    return 0;
+}
