abc6b1edd63963d5408921df35b541a0054f3501
ualiurrahat <ualiurrahat25@gmail.com>
2024-01-04 23:56:12 +0600

    code added

--- 10. Stacks and Queues/8_stackUsingLinkedList.cpp
+++ 10. Stacks and Queues/8_stackUsingLinkedList.cpp
@@ -78,15 +78,6 @@ public:
     void push(T element)
     {
         Node<T> *newNode = new Node<T>(element);
-        // checking if head is null.
-        if (head == nullptr)
-        {
-            // newNode is first element of stack. update head;
-            head = newNode;
-            // update size of stack
-            size++;
-            return;
-        }
         // instead of adding node at last , we are adding new Node at 0th postion
         // this is helpful for pop() function
         // means if Stack first two elements are 10,20
@@ -109,3 +100,28 @@ public:
         return head->data;
     }
 };
+int main()
+{
+    Stack<char> s;
+    s.push('r');
+    s.push('a');
+    s.push('h');
+    s.push('a');
+    s.push('t');
+    cout << s.getSize() << endl; // 5
+
+    cout << s.top() << endl;     // t
+    cout << s.pop() << endl;     // t
+    cout << s.pop() << endl;     // a
+    cout << s.pop() << endl;     // h
+    cout << s.getSize() << endl; // 2
+
+    cout << s.isEmpty() << endl; // 0
+    cout << s.pop() << endl;     // a
+    cout << s.pop() << endl;     // r
+    cout << s.top() << endl;     // stack is empty!
+    cout << s.pop() << endl;     // stack is empty!
+    cout << s.isEmpty() << endl; // true->1
+
+    return 0;
+}
