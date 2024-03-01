f2a537f54aa20cc9009eaf234b7c6389135a3e20
ualiurrahat <ualiurrahat25@gmail.com>
2024-03-01 20:59:35 +0600

    optimal solution coded

--- 08_Arrays/Hard/34_majorityElement2.cpp
+++ 08_Arrays/Hard/34_majorityElement2.cpp
@@ -12,6 +12,7 @@ Explanation: The frequency of ‘1’ is 5, which is greater than floor(N / 3), 
 #include <vector>
 #include <map>
 #include <algorithm>
+#include <climits>
 using namespace std;
 
 // brute force solution
@@ -95,6 +96,63 @@ vector<int> majorityElementBetter(vector<int> v)
     // for unordered map: T:O(n)[avg case] as it takes O(1) time for map operation
     // for unordered map: T:O(n^2)[worst case] as it takes O(n) time for map operation.
 }
+
+// optimal solution
+vector<int> majorityElementOptimal(vector<int> v)
+{
+    int n = v.size(); // size of the array
+
+    int cnt1 = 0, cnt2 = 0; // counts
+    int el1 = INT_MIN;      // element 1
+    int el2 = INT_MIN;      // element 2
+
+    // applying the Extended Boyer Moore's Voting Algorithm:
+    for (int i = 0; i < n; i++)
+    {
+        if (cnt1 == 0 && el2 != v[i])
+        {
+            cnt1 = 1;
+            el1 = v[i];
+        }
+        else if (cnt2 == 0 && el1 != v[i])
+        {
+            cnt2 = 1;
+            el2 = v[i];
+        }
+        else if (v[i] == el1)
+            cnt1++;
+        else if (v[i] == el2)
+            cnt2++;
+        else
+        {
+            cnt1--, cnt2--;
+        }
+    }
+
+    vector<int> ls; // list of answers
+
+    // Manually check if the stored elements in
+    // el1 and el2 are the majority elements:
+    cnt1 = 0, cnt2 = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (v[i] == el1)
+            cnt1++;
+        if (v[i] == el2)
+            cnt2++;
+    }
+
+    int mini = int(n / 3) + 1;
+    if (cnt1 >= mini)
+        ls.push_back(el1);
+    if (cnt2 >= mini)
+        ls.push_back(el2);
+
+    sort(ls.begin(), ls.end()); // TC --> O(2*log2) ~ O(1);
+
+    return ls;
+    // T:O(2N), S:O(1)
+}
 int main()
 {
     vector<int> arr = {2, 2, 1, 3, 1, 1, 3, 1, 1};
@@ -105,8 +163,10 @@ int main()
     }
     cout << endl;
     // vector<int> ans = majorityElementBrute(arr);
-    vector<int> ans = majorityElementBetter(arr);
-    cout << "majority elements are: ";
+    // vector<int> ans = majorityElementBetter(arr);
+    vector<int> ans = majorityElementOptimal(arr);
+    cout
+        << "majority elements are: ";
     for (int i = 0; i < ans.size(); i++)
     {
         cout << ans[i] << " ";
